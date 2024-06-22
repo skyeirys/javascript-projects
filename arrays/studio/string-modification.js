ae69d6662fb7639757d6975915241415d1e9eab6
Hennessy <henri@henritrinh.com>
2024-06-22 16:08:44 -0500

    Array Studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,7 +3,7 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let newStr = str.slice(3).concat(str.slice(3));
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
